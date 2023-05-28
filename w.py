a70817c40f2124423f970ee7bf71af244eea451c
wh1tePigeon <90470778+wh1tePigeon@users.noreply.github.com>
2023-05-29 01:12:54 +0300

    Update w.py

--- w.py
+++ w.py
@@ -3,10 +3,10 @@ import torch
 from pathlib import Path
 
 
-def transcribe_w(path):
+def transcribe_w(path, script_dir):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-    path_to_w = str(Path("models", "eng"))
+    path_to_w = str(Path(script_dir, "models", "eng"))
     model = whisper.load_model(name="medium", download_root=path_to_w)
     model.to(device)
 
